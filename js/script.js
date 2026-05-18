080c8b82ff71e6e0d60b0822667567d83e363e96
vasudevsayan <isayanvasudev7@gmail.com>
2026-05-18 15:20:37 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// NAVBAR TOGGLER ANIMATION
+
+const menuToggle = document.getElementById("menuToggle");
+
+menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+});
